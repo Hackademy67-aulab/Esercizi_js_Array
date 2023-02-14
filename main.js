c1b6c15cb71c6f19fb54ff703045c16fbfb99eb8
Leonardo Colaleo <124148340+leon94H@users.noreply.github.com>
2023-02-14 22:38:23 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+
+
+// ARRAY - ESERCIZIO 1
+
+// let stringa = 'angolo bar a bologna';
+
+// function pal(frase) {
+// let stringaSenzaSpazi = frase.replace(/\W/g,"");
+// let stringaInvertita = stringaSenzaSpazi.split('').reverse().join('');
+// console.log(stringaInvertita);
+
+// if(stringaSenzaSpazi == stringaInvertita)
+// {
+//     console.log("TRUE");
+// }
+// else
+// {
+//     console.log("FALSE");
+// }
+// }
+
+// let palindromo = pal(stringa);
+
+// ARRAY - ESERCIZIO 2
+
+
+// let numbers = [3,7,12]
+// let numbers2 = [5,6,20]
+// function maggiore(array) {
+//   let maxnumb = Math.max(...array)
+//   return maxnumb 
+
+
+// }
+
+// console.log(maggiore(numbers2));
+
+
+// ARRAY - ESERCIZIO 3
+
+// let numbers = [2,8,4,6,20,14,17,22,1,40]
+
+// numbers.sort(function(a, b) {
+//     return b - a
+// })
+
+// console.log(numbers);
+
+
+// ARRAY - ESERCIZIO 4
+
+
+let arr = [4,11,'Napoleone','Leoni'];
+
+let incluso = arr.includes['Napoleone'];
+
+console.log(incluso);
